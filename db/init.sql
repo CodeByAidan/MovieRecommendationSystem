39e5c9da1dda89890e633d98314efe28ab363787
livxy <67598470+livxy@users.noreply.github.com>
2023-07-17 12:33:28 -0400

    Updated SQL `init.sql` file, added 2 new tables + updated 1.

--- db/init.sql
+++ db/init.sql
@@ -1,10 +1,28 @@
+SELECT 'Running init.sql';
+CREATE DATABASE IF NOT EXISTS movie_recommendation CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE movie_recommendation;
 
-CREATE TABLE User(
+CREATE TABLE IF NOT EXISTS User(
     id INT AUTO_INCREMENT,
-    name VARCHAR(100),
-    email VARCHAR(100) UNIQUE,
-    password VARCHAR(100),
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
     PRIMARY KEY(id)
-);
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+CREATE TABLE IF NOT EXISTS movies (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(200) NOT NULL,
+    genre VARCHAR(100),
+    director VARCHAR(100),
+    rating FLOAT DEFAULT 0
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS ratings (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    movie_id INT,
+    user_id INT,
+    rating INT NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movies(id),
+    FOREIGN KEY (user_id) REFERENCES User(id)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
