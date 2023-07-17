17c8d96e5afae3a4257f8d12de8ff6b11905589e
livxy <67598470+livxy@users.noreply.github.com>
2023-07-17 12:35:26 -0400

    Formatting `init.sql` file.

--- db/init.sql
+++ db/init.sql
@@ -1,28 +1,40 @@
 SELECT 'Running init.sql';
-CREATE DATABASE IF NOT EXISTS movie_recommendation CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE DATABASE IF NOT EXISTS movie_recommendation CHARACTER SET utf8mb4 COLLATE
+    utf8mb4_unicode_ci;
+
 USE movie_recommendation;
 
-CREATE TABLE IF NOT EXISTS User(
-    id INT AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
+CREATE TABLE IF NOT EXISTS user
+(
+    id       INT auto_increment,
+    name     VARCHAR(100) NOT NULL,
+    email    VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
     PRIMARY KEY(id)
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+)
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS movies (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(200) NOT NULL,
-    genre VARCHAR(100),
+CREATE TABLE IF NOT EXISTS movies
+(
+    id       INT auto_increment PRIMARY KEY,
+    title    VARCHAR(200) NOT NULL,
+    genre    VARCHAR(100),
     director VARCHAR(100),
-    rating FLOAT DEFAULT 0
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    rating   FLOAT DEFAULT 0
+)
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS ratings (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS ratings
+(
+    id       INT auto_increment PRIMARY KEY,
     movie_id INT,
-    user_id INT,
-    rating INT NOT NULL,
+    user_id  INT,
+    rating   INT NOT NULL,
     FOREIGN KEY (movie_id) REFERENCES movies(id),
-    FOREIGN KEY (user_id) REFERENCES User(id)
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    FOREIGN KEY (user_id) REFERENCES user(id)
+)
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci; 
