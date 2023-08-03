1b6284c981ee853453e86c66b50648d25716fdf9
livxy <67598470+livxy@users.noreply.github.com>
2023-08-03 17:37:07 -0400

    Commented out code in MovieRecommendationSystemTests class.

--- src/test/java/com/movie/recommendation/MovieRecommendationSystemTests.java
+++ src/test/java/com/movie/recommendation/MovieRecommendationSystemTests.java
@@ -1,13 +1,13 @@
-package com.movie.recommendation;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class MovieRecommendationSystemTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.movie.recommendation;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class MovieRecommendationSystemTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
